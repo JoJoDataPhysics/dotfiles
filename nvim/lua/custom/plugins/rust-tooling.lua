487d05630ed268fccc3b3e61ee4307d0ebba69c9
joerg <joerg@example.com>
2024-09-29 22:50:02 +0200

    new: buffer explorer, lazygit, remove rust plugin

--- nvim/lua/custom/plugins/rust-tooling.lua
+++ nvim/lua/custom/plugins/rust-tooling.lua
@@ -19,14 +19,13 @@ return {
       }
     end,
   },
-
-  {
-    'rust-lang/rust.vim',
-    ft = 'rust',
-    init = function()
-      vim.g.rustfmt_autosave = 1
-    end,
-  },
+  -- {
+  --   'rust-lang/rust.vim',
+  --   ft = 'rust',
+  --   init = function()
+  --     vim.g.rustfmt_autosave = 1
+  --   end,
+  -- },
   {
     'saecki/crates.nvim',
     ft = { 'toml' },
