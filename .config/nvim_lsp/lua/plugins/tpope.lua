9543c3af302132783e645c7855e3d5abfd415808
joerg <joerg@example.com>
2024-10-18 18:03:20 +0200

    removed redundant tpope surround

--- .config/nvim_lsp/lua/plugins/tpope.lua
+++ .config/nvim_lsp/lua/plugins/tpope.lua
@@ -1,7 +1,7 @@
 return {
     { 'tpope/vim-fugitive' },
     { 'tpope/vim-speeddating' },
-    { 'tpope/vim-surround' },
+    -- { 'tpope/vim-surround' },
     { 'tpope/vim-sensible' },
     { 'tpope/vim-scriptease' },
     { 'tpope/vim-repeat' },
